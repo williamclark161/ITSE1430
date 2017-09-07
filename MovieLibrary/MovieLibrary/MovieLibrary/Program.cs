28cecc56d9d0d2e1c10b9354d344e5fda5760450
williamclark161 <william.clark161@my.tccd.edu>
2017-09-07 16:53:24 -0500

    Movie Library 090717

--- MovieLibrary/MovieLibrary/MovieLibrary/Program.cs
+++ MovieLibrary/MovieLibrary/MovieLibrary/Program.cs
@@ -27,43 +27,9 @@ namespace MovieLibrary {
             Console.WriteLine();
 
             // Display menu and recieve user choice
-            string choice = displayMenu();
-            
-            // Perform function of menu choice
-            while (choice != "Q")
-            {
-                switch (choice)
-                {
-                    case ("L"):
-                        listMovies(movieLibrary);
-                        break;
+            string userChoice = displayMenu();
+            performChoice(userChoice,movieLibrary, totalMovies);
 
-                    case ("l"):
-                        listMovies(movieLibrary);
-                        break;
-
-                    case ("A"):
-                        totalMovies = addMovie(movieLibrary, totalMovies);
-                        break;
-
-                    case ("a"):
-                        totalMovies = addMovie(movieLibrary, totalMovies);
-                        break;
-
-                    case ("R"):
-                        totalMovies = removeMovie(movieLibrary, totalMovies);
-                        break;
-
-                    case ("r"):
-                        totalMovies = removeMovie(movieLibrary, totalMovies);
-                        break;
-
-                    default:
-                        Console.WriteLine("Invalid Choice!!  Please enter first letter of the following choices");
-                        displayMenu();
-                        break;
-                }
-            }  
         }
 
         // Function to display menu
@@ -74,11 +40,82 @@ namespace MovieLibrary {
             Console.WriteLine("3. (R)emove Movie");
             Console.WriteLine("4. (Q)uit");
             Console.Write("What would you like to do?");
-            return Console.ReadLine(); ;
+            return Console.ReadLine();
         }
 
-        // Function to Ad a Movie to the Library
-        static int removeMovie(string [] movieLibrary, int movieCount)
+        // Perform function of menu choice
+        static void performChoice(string choice, string [] movies, int libraryCount)
+        {
+            while (choice != "Q")
+            {
+                switch (choice)
+                {
+                    case ("L"):
+                        listMovies(movies, libraryCount);
+                        break;
+
+                    case ("l"):
+                        listMovies(movies, libraryCount);
+                        break;
+
+                    case ("A"):
+                        libraryCount = addMovie(movies, libraryCount);
+                        break;
+
+                    case ("a"):
+                        libraryCount = addMovie(movies, libraryCount);
+                        break;
+
+                    case ("R"):
+                        libraryCount = removeMovie(movies, libraryCount);
+                        break;
+
+                    case ("r"):
+                        libraryCount = removeMovie(movies, libraryCount);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Choice!!  Please enter first letter of the following choices");
+                        displayMenu();
+                        break;
+                }
+                choice = displayMenu();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Thank you for using the CocoaVision Movie Library.  Crestworld Studios");
+        }
+        
+        // Function to Display the Entire Movie Library
+        static void listMovies(string [] library, int movieCount)
+        {
+            Console.WriteLine();
+            Console.WriteLine("List of movies in the library");
+            for (int position = 0; position < movieCount; position++)
+            {
+                Console.WriteLine(library[position]);
+            }
+        }
+
+        // Function to Add a Movie to the Library
+        static int addMovie(string [] library, int movieCount)
+        {
+            Console.WriteLine();
+            Console.Write("Enter The Name Of The Movie You Want To Add: ");
+            bool added = false;
+            string movie = Console.ReadLine();
+            for (int position = 0; !added; position++)
+            {
+                if (library[position] == "")
+                {
+                    library[position] = movie;
+                    added = true;
+                }
+            }
+            return movieCount++;
+        }
+
+        // Function to Remove a Movie from the Library
+        static int removeMovie(string [] library, int movieCount)
         {
             Console.WriteLine();
             if (movieCount > 0)
@@ -87,18 +124,29 @@ namespace MovieLibrary {
                 string movie = Console.ReadLine();
                 for (int position = 0; position < movieCount; position++)
                 {
-                    if (movieLbrary[position] == movie)
+                    if (library[position] == movie)
                     {
-                        if ()
+                        if (library[position+1] != "")
+                        {
+                            library[position] = library[position+1];
+                        }
+                        else
+                        {
+                            library[position] = "";
+                        }
+                        movieCount--;
+                    }
+                    else
+                    {
+                        Console.WriteLine(movie + " is not in movie library");
                     }
                 }
+                return movieCount;
             }
             else
             {
                 Console.WriteLine("There are no movies in the library to remove");
             }
-            
-
         }
     }
 }
